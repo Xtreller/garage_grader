a5714b3224f706ef8fb41850b875e79de501732a
Xtrell <vladsito@gmail.com>
2023-07-28 16:30:10 +0300

    Garage-details

--- src/app/features/components/garage/details/details.component.ts
+++ src/app/features/components/garage/details/details.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import Garage from 'src/app/features/interfaces/Garage/garage.interface';
+import { GarageService } from 'src/app/features/services/Garage/garage.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  garage: Garage;
+
+  constructor(private garageService: GarageService, private route: ActivatedRoute) { }
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.garageService.getGarage(id).subscribe((response: any) => {
+      console.log('garage',response.data);
+      this.garage = response.data;
+    })
+  }
 
 }
--- src/app/features/interfaces/Garage/garage.interface.ts
+++ src/app/features/interfaces/Garage/garage.interface.ts
@@ -1,6 +1,8 @@
 export default interface Garage{
   name:string;
   description:string;
+  email:string,
+  type:string,
   services:string,
   address:string,
   town:string,
